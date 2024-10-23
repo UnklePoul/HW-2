7c2f33a63ea8a0363fa3e32942cb0bff5278bcdd
«Poul» <overlord.est@mail.ru>
2024-10-23 13:49:27 +1200

    Z0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,70 @@
 public class Main {
     public static void main(String[] args) {
 
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog -= 3.5 ;
+        cat -=  1.6;
+        paper -=  7639;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend /=7 ;
+        System.out.println(friend);
+
+        var frog = 3.5;
+
+
+        System.out.println("Значение пременной frog:" + frog);
+
+        frog *= 10;
+
+
+        System.out.println("Значение пременной frog:" + frog);
+
+        frog /= 3.5 ;
+
+
+        System.out.println("Значение пременной frog:" + frog);
+
+        frog +=4 ;
+
+
+        System.out.println("Значение пременной frog:" + frog);
+
+        var boxer1Weight = 78.2 ;
+        var boxer2Weight = 82.7;
+
+        var totalWeight = boxer1Weight + boxer2Weight ;
+        var weightDifference = boxer2Weight - boxer1Weight ;
+        System.out.println("Обща масса бойцов " + totalWeight );
+        System.out.println("Разница между массами бойцов " + weightDifference );
+
+        var reminder = boxer2Weight % boxer1Weight  ;
+        System.out.println("Остаток от деления "+reminder);
+
+
+
+
         var  workTime = 640 ;
         var tymePerDay = 8 ;
 
